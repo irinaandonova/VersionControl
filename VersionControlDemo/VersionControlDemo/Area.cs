a856144eefbf37b0a4a9ff93db61ba4178fc472a
irinaandonova <irinaandonova97@gmail.com>
2022-11-05 18:40:53 +0200

    Add constructor and get area method to Area class

--- VersionControlDemo/VersionControlDemo/Area.cs
+++ VersionControlDemo/VersionControlDemo/Area.cs
@@ -6,6 +6,16 @@ namespace VersionControlDemo
 {
     internal class Area
     {
+        public double side { get; set; }
+        public double area { get; set; }
 
+        public Area(double side)
+        {
+            this.side = side;
+        }
+        public void getArea()
+        {
+            area = side * side;
+        }
     }
 }

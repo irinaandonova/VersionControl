dbd7537ac9c554036ca4b128af5b9796fe7044b6
irinaandonova <irinaandonova97@gmail.com>
2022-11-05 16:14:37 +0200

    Change message written in console

--- VersionControlDemo/VersionControlDemo/Program.cs
+++ VersionControlDemo/VersionControlDemo/Program.cs
@@ -6,7 +6,7 @@ namespace VersionControlDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, John!");
         }
     }
 }

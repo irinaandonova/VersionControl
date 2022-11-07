9b69c187ef206b36a9cd017d954125cd0d565a61
irinaandonova <irinaandonova97@gmail.com>
2022-11-07 09:50:50 +0200

    Define Person class

--- VersionControlDemo/VersionControlDemo/Person.cs
+++ VersionControlDemo/VersionControlDemo/Person.cs
@@ -6,5 +6,16 @@ namespace VersionControlDemo
 {
     internal class Person
     {
+        public string name;
+
+        Person(string name)
+        {
+            this.name = name;
+        }
+
+        public void GreetPreson()
+        {
+            Console.WriteLine($"Hello, {name}");
+        }
     }
 }
